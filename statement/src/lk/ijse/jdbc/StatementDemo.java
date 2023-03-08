a4f02a22314abfa72544be1660c46815efa02f45
danu <danujagreru@gmail.com>
2023-03-08 15:21:20 +0530

    done statement

--- statement/src/lk/ijse/jdbc/StatementDemo.java
+++ statement/src/lk/ijse/jdbc/StatementDemo.java
@@ -5,10 +5,7 @@ package lk.ijse.jdbc;
     @created 3/8/23 - 2:04 PM   
 */
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.Properties;
 
 public class StatementDemo {
@@ -63,12 +60,68 @@ public class StatementDemo {
         System.out.println(affectedRows > 0 ? "customer updated!" : "customer not updated!");
     }
 
+    private static void searchById() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(URL, props);
+        Statement statement = con.createStatement();
+
+        String sql = "SELECT * FROM Customer WHERE id = \"C001\"";
+
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        if(resultSet.next()) {
+            String id = resultSet.getString(1);
+            String name = resultSet.getString(2);
+            String address = resultSet.getString(3);
+            double salary = resultSet.getDouble(4);
+
+            System.out.println(id + " - " + name + " - " + address + " - " + salary);
+
+        }
+    }
+
+    private static void searchAll() throws ClassNotFoundException, SQLException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection con = DriverManager.getConnection(URL, props);
+        Statement statement = con.createStatement();
+
+        String sql = "SELECT * FROM Customer";
+        ResultSet resultSet = statement.executeQuery(sql);
+
+        while(resultSet.next()) {
+            String id = resultSet.getString(1);
+            String name = resultSet.getString(2);
+            String address = resultSet.getString(3);
+            double salary = resultSet.getDouble(4);
+            System.out.println(id + " - " + name + " - " + address + " - " + salary);
+        }
+        /*resultSet.next();
+        String id = resultSet.getString(1);
+        String name = resultSet.getString(2);
+        String address = resultSet.getString(3);
+        double salary = resultSet.getDouble(4);
+        System.out.println(id + " - " + name + " - " + address + " - " + salary);
+
+        resultSet.next();
+        String id2 = resultSet.getString(1);
+        String name2 = resultSet.getString(2);
+        String address2 = resultSet.getString(3);
+        double salary2 = resultSet.getDouble(4);
+        System.out.println(id2 + " - " + name2 + " - " + address2 + " - " + salary2);*/
+
+        con.close();
+    }
+
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
 //        insertCustomer();
 
 //        deleteCustomer();
 
         updateCustomer();
+
+//        searchById();
+
+        searchAll();
     }
 
 }
