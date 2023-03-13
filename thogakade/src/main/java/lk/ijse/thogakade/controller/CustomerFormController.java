b1368e4f0046201d563da1aaff42643b66d75a1f
danu <danujagreru@gmail.com>
2023-03-13 11:04:00 +0530

    customer CRUD ok

--- thogakade/src/main/java/lk/ijse/thogakade/controller/CustomerFormController.java
+++ thogakade/src/main/java/lk/ijse/thogakade/controller/CustomerFormController.java
@@ -7,11 +7,23 @@ package lk.ijse.thogakade.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 
+import java.sql.*;
+import java.util.Properties;
+
 public class CustomerFormController {
+    private final static String URL = "jdbc:mysql://localhost:3306/ThogaKade";
+    private final static Properties props = new Properties();
+
+    static {
+        props.setProperty("user", "root");
+        props.setProperty("password", "Danu25412541@");
+    }
+
     @FXML
     private TableColumn<?, ?> colAction;
 
@@ -58,18 +70,74 @@ public class CustomerFormController {
     }
 
     @FXML
-    void btnSaveOnAction(ActionEvent event) {
+    void btnSaveOnAction(ActionEvent event) throws SQLException {
+        String id = txtId.getText();
+        String name = txtName.getText();
+        String address = txtAddress.getText();
+        double salary = Double.parseDouble(txtSalary.getText());
 
+        try (Connection con = DriverManager.getConnection(URL, props)) {
+            String sql = "INSERT INTO Customer(id, name, address, salary) VALUES(?, ?, ?, ?)";
+
+            PreparedStatement pstm = con.prepareStatement(sql);
+            pstm.setString(1, id);
+            pstm.setString(2, name);
+            pstm.setString(3, address);
+            pstm.setDouble(4, salary);
+
+            int affectedRows = pstm.executeUpdate();
+            if(affectedRows > 0) {
+                new Alert(Alert.AlertType.CONFIRMATION, "Customer Added!!").show();
+            }
+        }
     }
 
     @FXML
-    void btnUpdateOnAction(ActionEvent event) {
+    void btnUpdateOnAction(ActionEvent event) throws SQLException {
+        String id = txtId.getText();
+        String name = txtName.getText();
+        String address = txtAddress.getText();
+        double salary = Double.parseDouble(txtSalary.getText());
 
+        try(Connection con = DriverManager.getConnection(URL, props)) {
+            String sql = "UPDATE Customer SET name = ?, address = ?, salary = ? WHERE id = ?";
+
+            PreparedStatement pstm = con.prepareStatement(sql);
+            pstm.setString(1, name);
+            pstm.setString(2, address);
+            pstm.setDouble(3, salary);
+            pstm.setString(4, id);
+
+            if(pstm.executeUpdate() > 0) {
+                new Alert(Alert.AlertType.CONFIRMATION, "Customer Updated!!").show();
+            }
+        }
     }
 
     @FXML
-    void codeSearchOnAction(ActionEvent event) {
+    void codeSearchOnAction(ActionEvent event) throws SQLException {
+        String id = txtId.getText();
 
+        try(Connection con = DriverManager.getConnection(URL, props)) {
+            String sql = "SELECT * FROM Customer WHERE id = ?";
+
+            PreparedStatement pstm = con.prepareStatement(sql);
+            pstm.setString(1, id);
+
+            ResultSet resultSet = pstm.executeQuery();
+
+            if(resultSet.next()) {
+                String cus_id = resultSet.getString(1);
+                String cus_name = resultSet.getString(2);
+                String cus_address = resultSet.getString(3);
+                double cus_salary = resultSet.getDouble(4);
+
+                txtId.setText(cus_id);
+                txtName.setText(cus_name);
+                txtAddress.setText(cus_address);
+                txtSalary.setText(String.valueOf(cus_salary));
+            }
+        }
     }
 
 }
