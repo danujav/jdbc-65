5d31df7543f9a0b924cf6f45129b6cdfa337333e
danu <danujagreru@gmail.com>
2023-03-13 09:33:18 +0530

    complete dashboard fxml

---
+++ thogakade/src/main/java/lk/ijse/thogakade/controller/DashboardFormController.java
@@ -0,0 +1,30 @@
+package lk.ijse.thogakade.controller;
+
+/*
+    @author DanujaV
+    @created 3/13/23 - 9:22 AM   
+*/
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
+public class DashboardFormController {
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    void btnCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnItemOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPlaceOrderOnAction(ActionEvent event) {
+
+    }
+}
