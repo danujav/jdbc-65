ed76aeab3b6d1d8795e10eb043a845a642d654f3
danu <danujagreru@gmail.com>
2023-03-13 09:39:23 +0530

    run dashboard

---
+++ thogakade/src/main/java/lk/ijse/thogakade/controller/Launcher.java
@@ -0,0 +1,24 @@
+package lk.ijse.thogakade.controller;
+
+/*
+    @author DanujaV
+    @created 3/13/23 - 9:35 AM   
+*/
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Launcher extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/dashboard_form.fxml"));
+        stage.setTitle("Dashboard Form");
+        stage.centerOnScreen();
+        stage.setScene(new Scene(root));
+
+        stage.show();
+    }
+}
