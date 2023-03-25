d9e05ad1a64ebd2ff1b72eb9fedb151d3252eec0
danu <danujagreru@gmail.com>
2023-03-25 13:13:17 +0530

    setOnAction and netTotal

--- 04-thogakade/src/main/java/lk/ijse/thogakade/controller/PlaceOrderFormController.java
+++ 04-thogakade/src/main/java/lk/ijse/thogakade/controller/PlaceOrderFormController.java
@@ -9,9 +9,13 @@ import com.jfoenix.controls.JFXComboBox;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Cursor;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
@@ -29,7 +33,9 @@ import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.EventListener;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class PlaceOrderFormController implements Initializable {
@@ -85,6 +91,8 @@ public class PlaceOrderFormController implements Initializable {
     private TextField txtQty;
 
     private ObservableList<PlaceOrderTM> obList = FXCollections.observableArrayList();
+    @FXML
+    private Label lblNetTotal;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -146,29 +154,61 @@ public class PlaceOrderFormController implements Initializable {
         int qty = Integer.parseInt(txtQty.getText());
         double unitPrice = Double.parseDouble(lblUnitPrice.getText());
         double total = qty * unitPrice;
-        Button btn = new Button("Remove");
+        Button btnRemove = new Button("Remove");
+        btnRemove.setCursor(Cursor.HAND);
 
-        if(!obList.isEmpty()) {
+        setRemoveBtnOnAction(btnRemove); /* set action to the btnRemove */
+
+        if (!obList.isEmpty()) {
             for (int i = 0; i < tblOrderCart.getItems().size(); i++) {
-                if(colItemCode.getCellData(i).equals(code)) {
-                    qty += (int)colQty.getCellData(i);
+                if (colItemCode.getCellData(i).equals(code)) {
+                    qty += (int) colQty.getCellData(i);
                     total = qty * unitPrice;
 
                     obList.get(i).setQty(qty);
                     obList.get(i).setTotal(total);
 
                     tblOrderCart.refresh();
+                    calculateNetTotal();
                     return;
                 }
             }
         }
 
-        PlaceOrderTM tm = new PlaceOrderTM(code, description, qty, unitPrice, total, btn);
+        PlaceOrderTM tm = new PlaceOrderTM(code, description, qty, unitPrice, total, btnRemove);
 
         obList.add(tm);
         tblOrderCart.setItems(obList);
+        calculateNetTotal();
 
         txtQty.setText("");
+    }
+    private void calculateNetTotal() {
+        double netTotal = 0.0;
+        for (int i = 0; i < tblOrderCart.getItems().size(); i++) {
+            double total  = (double) colTotal.getCellData(i);
+            netTotal += total;
+        }
+        lblNetTotal.setText(String.valueOf(netTotal));
+    }
+
+    private void setRemoveBtnOnAction(Button btn) {
+        btn.setOnAction((e) -> {
+            ButtonType yes = new ButtonType("Yes", ButtonBar.ButtonData.OK_DONE);
+            ButtonType no = new ButtonType("No", ButtonBar.ButtonData.CANCEL_CLOSE);
+
+            Optional<ButtonType> result = new Alert(Alert.AlertType.INFORMATION, "Are you sure to remove?", yes, no).showAndWait();
+
+            if (result.orElse(no) == yes) {
+
+                int index = tblOrderCart.getSelectionModel().getSelectedIndex();
+                obList.remove(index);
+
+                tblOrderCart.refresh();
+                calculateNetTotal();
+            }
+
+        });
     }
 
     void setCellValueFactory() {
