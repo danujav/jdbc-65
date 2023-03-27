2a55102d5341c901e3ed9ad6c66c94210694e324
danu <danujagreru@gmail.com>
2023-03-27 12:07:41 +0530

    crud util

--- 04-thogakade/src/main/java/lk/ijse/thogakade/model/ItemModel.java
+++ 04-thogakade/src/main/java/lk/ijse/thogakade/model/ItemModel.java
@@ -8,6 +8,7 @@ package lk.ijse.thogakade.model;
 import lk.ijse.thogakade.db.DBConnection;
 import lk.ijse.thogakade.dto.CartDTO;
 import lk.ijse.thogakade.dto.Item;
+import lk.ijse.thogakade.util.CrudUtil;
 
 import java.sql.*;
 import java.util.ArrayList;
@@ -23,89 +24,47 @@ public class ItemModel {
         props.setProperty("password", "Danu25412541@");
     }
     public static boolean save(String code, String description, double unitPrice, int qtyOnHand) throws SQLException {
-        try(Connection con = DriverManager.getConnection(URL, props)) {
-            String sql = "INSERT INTO Item(code, description, unitPrice, qtyOnHand) VALUES(?, ?, ?, ?)";
-            PreparedStatement pstm = con.prepareStatement(sql);
-            pstm.setString(1, code);
-            pstm.setString(2, description);
-            pstm.setDouble(3, unitPrice);
-            pstm.setInt(4, qtyOnHand);
+        String sql = "INSERT INTO Item(code, description, unitPrice, qtyOnHand) VALUES(?, ?, ?, ?)";
 
-            int affectedRows = pstm.executeUpdate();
-            if(affectedRows > 0) {
-                return true;
-            } else {
-                return false;
-            }
-        }
+        return CrudUtil.execute(sql, code, description, unitPrice, qtyOnHand);
+
+
+
     }
-
-    /*public static boolean update(String code, String description, double unitPrice, int qtyOnHand) throws SQLException {
-        try(Connection con = DriverManager.getConnection(URL, props)) {
-            String sql = "UPDATE Item SET description = ?, unitPrice = ?, qtyOnHand = ? WHERE code = ?";
-            PreparedStatement pstm = con.prepareStatement(sql);
-            pstm.setString(1, description);
-            pstm.setDouble(2, unitPrice);
-            pstm.setInt(3, qtyOnHand);
-            pstm.setString(4, code);
-
-            return pstm.executeUpdate() > 0;
-        }
-
-    }*/
     public static boolean update(Item item) throws SQLException {
-        try(Connection con = DriverManager.getConnection(URL, props)) {
-            String sql = "UPDATE Item SET description = ?, unitPrice = ?, qtyOnHand = ? WHERE code = ?";
-            PreparedStatement pstm = con.prepareStatement(sql);
-            pstm.setString(1, item.getDescription());
-            pstm.setDouble(2, item.getUnitPrice());
-            pstm.setInt(3, item.getQtyOnHand());
-            pstm.setString(4, item.getCode());
-
-            return pstm.executeUpdate() > 0;
-        }
+        String sql = "UPDATE Item SET description = ?, unitPrice = ?, qtyOnHand = ? WHERE code = ?";
+        return CrudUtil.execute(sql, item.getDescription(), item.getUnitPrice(), item.getQtyOnHand(), item.getCode());
     }
 
     public static Item search(String code) throws SQLException {
-        try(Connection con = DriverManager.getConnection(URL, props)) {
-            String sql = "SELECT * FROM Item WHERE code = ?";
+        String sql = "SELECT * FROM Item WHERE code = ?";
+        ResultSet resultSet = CrudUtil.execute(sql, code);
 
-            PreparedStatement pstm = con.prepareStatement(sql);
-            pstm.setString(1, code);
+        if(resultSet.next()) {
+            String item_code = resultSet.getString(1);
+            String item_description = resultSet.getString(2);
+            Double item_unit_price = resultSet.getDouble(3);
+            Integer item_qty_on_hand = resultSet.getInt(4);
 
-            ResultSet resultSet = pstm.executeQuery();
-            if(resultSet.next()) {
-                String item_code = resultSet.getString(1);
-                String item_description = resultSet.getString(2);
-                Double item_unit_price = resultSet.getDouble(3);
-                Integer item_qty_on_hand = resultSet.getInt(4);
-
-                return new Item(item_code, item_description, item_unit_price, item_qty_on_hand);
-            }
-            return null;
+            return new Item(item_code, item_description, item_unit_price, item_qty_on_hand);
         }
+        return null;
     }
 
     public static List<Item> searchAll() throws SQLException {
-        try(Connection con = DriverManager.getConnection(URL, props)) {
-            String sql = "SELECT * FROM Item";
+        ResultSet resultSet = CrudUtil.execute("SELECT * FROM Item");
+        List<Item> dataList = new ArrayList<>();
 
-            PreparedStatement pstm = con.prepareStatement(sql);
-            ResultSet resultSet = pstm.executeQuery();
+        while (resultSet.next()) {
+            String item_code = resultSet.getString(1);
+            String item_description = resultSet.getString(2);
+            Double item_unit_price = resultSet.getDouble(3);
+            Integer item_qty_on_hand = resultSet.getInt(4);
 
-            List<Item> dataList = new ArrayList<>();
-
-            while (resultSet.next()) {
-                String item_code = resultSet.getString(1);
-                String item_description = resultSet.getString(2);
-                Double item_unit_price = resultSet.getDouble(3);
-                Integer item_qty_on_hand = resultSet.getInt(4);
-
-                var item = new Item(item_code, item_description, item_unit_price, item_qty_on_hand);
-                dataList.add(item);
-            }
-            return dataList;
+            var item = new Item(item_code, item_description, item_unit_price, item_qty_on_hand);
+            dataList.add(item);
         }
+        return dataList;
     }
 
     public static boolean delete(String code) throws SQLException {
